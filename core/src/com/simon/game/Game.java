0f05b1b80a017ab81494632f26f63e5b570f721a
RED-Pixl <108759893+RED-Pixl@users.noreply.github.com>
2023-06-08 14:17:09 +0200

    Added a character

--- core/src/com/simon/game/Game.java
+++ core/src/com/simon/game/Game.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -17,16 +18,31 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Game extends ApplicationAdapter {
 
+	SpriteBatch spriteBatch;
 	OrthogonalTiledMapRenderer renderer;
 	OrthographicCamera cam;
 	ExtendViewport viewport;
 
 	float viewportWidth,viewportHeight;
+	float delta;
+
+	int posX, posY;
+	int posXDelta, posYDelta;
+	Texture figureImg;
+	Rectangle figure;
 	
 	@Override
 	public void create () {
+		spriteBatch = new SpriteBatch();
+
 		viewportWidth = 32 * 30;
 		viewportHeight = 32 * 20;
+		delta = 0;
+
+		posX = 0;
+		posY = 0;
+		posXDelta = 0;
+		posYDelta = 0;
 
 		cam = new OrthographicCamera();
 		viewport = new ExtendViewport(viewportWidth, viewportHeight, cam);
@@ -37,6 +53,11 @@ public class Game extends ApplicationAdapter {
 		renderer = new OrthogonalTiledMapRenderer(map);
 		renderer.setView(cam);
 
+		// Setting up everything regarding the player-character
+
+		figure = new Rectangle(posX, posY, 16, 32);
+		figureImg = new Texture(Gdx.files.internal("Sprites/char.png"));
+
 		// Input Processor for managing the keyboard inputs
 		Gdx.input.setInputProcessor(new InputAdapter() {
 			@Override
@@ -44,6 +65,14 @@ public class Game extends ApplicationAdapter {
 				switch (keycode) {
 					case Input.Keys.ESCAPE:
 						Gdx.app.exit();
+					case Input.Keys.W:
+						posYDelta++;
+					case Input.Keys.S:
+						posYDelta--;
+					case Input.Keys.D:
+						posXDelta++;
+					case Input.Keys.A:
+						posXDelta--;
 				}
 				return super.keyDown(keycode);
 			}
@@ -51,7 +80,14 @@ public class Game extends ApplicationAdapter {
 			@Override
 			public boolean keyUp(int keycode) {
 				switch (keycode) {
-
+					case Input.Keys.W:
+						posYDelta--;
+					case Input.Keys.S:
+						posYDelta++;
+					case Input.Keys.D:
+						posXDelta--;
+					case Input.Keys.A:
+						posXDelta++;
 				}
 				return super.keyUp(keycode);
 			}
@@ -61,9 +97,36 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void render () {
 		ScreenUtils.clear(Color.BLACK);
+		viewport.apply();
 
 		renderer.setView(cam);
 		renderer.render();
+
+		spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+		spriteBatch.begin();
+
+		spriteBatch.draw(figureImg, posX, posY);
+
+		spriteBatch.end();
+
+		act();
+	}
+
+	private void act() {
+		delta += Gdx.graphics.getDeltaTime();
+		if (delta >= 1) {
+			delta %= 10;
+			posX += posXDelta;
+			posY += posYDelta;
+			if (posX > 32 * 30 - figure.width) {
+				posX = (int) (32 * 30 - figure.width);
+			} else if (posX < 0) {
+				posX = 0;
+			}
+			if (posY > 32 * 20 - figure.height) {
+				posY = (int) (32 * 20 - figure.height);
+			}
+		}
 	}
 
 	@Override
@@ -76,5 +139,7 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		renderer.dispose();
+		figureImg.dispose();
+		spriteBatch.dispose();
 	}
 }

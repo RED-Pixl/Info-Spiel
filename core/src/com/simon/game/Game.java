18e26904000eec8a03152df03f5d5d21c5fafb5d
RED-Pixl <108759893+RED-Pixl@users.noreply.github.com>
2024-04-04 19:22:00 +0200

    Updated the logo

--- core/src/com/simon/game/Game.java
+++ core/src/com/simon/game/Game.java
@@ -32,7 +32,7 @@ public class Game extends ApplicationAdapter {
 		viewport = new ExtendViewport(viewportWidth, viewportHeight, cam);
 
 		// Managing maps
-		mapId = 8;
+		mapId = 0;
 		maps = new Map[10];
 		maps[mapId] = MapFactory.create(mapId, cam, maps);
 		maps[mapId].enter();
